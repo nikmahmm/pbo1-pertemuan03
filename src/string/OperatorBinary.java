65bcab8ff9c16db9328db3d4269ca97eee1f4764
nikmahmm <nikmahmawaddah04@gmail.com>
2020-05-01 15:42:29 +0800

    Operator Binary Assignment

---
+++ src/string/OperatorBinary.java
@@ -0,0 +1,14 @@
+package string;
+
+public class OperatorBinary {
+
+    // operator (=) sebagai assaingment/penugasan
+    // menempatkan nilai kedalam variable
+    // baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+    String nama = "Nikmah Mawaddah Mufidah";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 3.89;
+
+    
+}
